23ccfe6c78a6ec348ef09163c3f65cb7219747ac
YeYerin <parkyerin30@gmail.com>
2019-03-20 23:46:03 +0900

    180320

--- 180320.js
+++ 180320.js
@@ -1,5 +1,5 @@
 function solution(answers) {
-    var answer = [];
+    var answer = [1,2,3,4,5];
     var a1 = [1, 2, 3, 4, 5];
     var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
     var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
