beff3a454047ca4197c2e3db8167f4e832b63e6e
YeYerin <46090857+YeYerin@users.noreply.github.com>
2019-03-22 23:28:54 +0900

    180320(수정)

--- 180320.js
+++ 180320.js
@@ -1,7 +1,9 @@
 function solution(answers) {
-    var answer = [1,2,3,4,5];
+    var answer = []; //반환값
     var a1 = [1, 2, 3, 4, 5];
     var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
     var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
+    
+    return answer;
+}
 }
