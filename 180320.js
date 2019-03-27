3240ef8c46fc36c0118b8e5bf83b9869a2afae9f
YeYerin <46090857+YeYerin@users.noreply.github.com>
2019-03-27 19:10:49 +0900

    Delete 180320.js

--- 180320.js
+++
@@ -1,9 +0,0 @@
-function solution(answers) {
-    var answer = []; //반환값
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    return answer;
-}
-}
