83bf0f21a13cc472fc5a33adcdc8ceb0cde6593a
YeYerin <46090857+YeYerin@users.noreply.github.com>
2019-03-31 00:37:24 +0900

    190331

--- 탑.js
+++ 탑.js
@@ -1,14 +1,16 @@
-
 function solution(heights) {
     let answer = [];
-    //맨앞에 위치하면 0, for문으로비교, 배열 거꾸로 되어있으니까 reserve해준다
-    for(let i=heights.length;i>0;i--){
+    
+    for(let i=heights.length-1;i>=0;i--){
         if(i==0) answer[i]=0;
         else{
-            for(let j=i-1;i>0;i--){
-                
+            for(let j=i-1;j>=0;j--){
+                if(heights[j]>heights[i]){
+                    answer[i]=j+1; break;
+                } else answer[i]=0;
             }
         }
+        
     }
     
     return answer;
